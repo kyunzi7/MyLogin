b0642a2358c2f9ca2ed35553e4e7fcda9ea3139d
Neuron-NB 47 <bobipermana78@gmail.com>
2018-10-23 04:48:19 +0700

    test

---
+++ app/src/main/java/com/devkotes/myapps/MainActivity.java
@@ -0,0 +1,26 @@
+package com.devkotes.myapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRegister = (Button) findViewById(R.id.register);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
