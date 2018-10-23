91ca5f3a38a585f32c8e633362523965d1172988
Neuron-NB 47 <bobipermana78@gmail.com>
2018-10-23 22:20:19 +0700

    test

---
+++ app/src/main/java/com/devkotes/myapps/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.devkotes.myapps;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    //Set waktu lama splashscreen
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+            private void finish(){
+
+            }
+        },splashInterval);
+    }
+}
